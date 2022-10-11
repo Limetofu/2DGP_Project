d0e75ed5fe279d38914360da3fb2cf35781fa06d
Limetofu <tkfkdgo6327@gmail.com>
2022-10-12 01:13:56 +0900

    box2d in pico2d

--- pico2d+jump.py
+++ pico2d+jump.py
@@ -16,6 +16,12 @@ JumpKeyPressed = 0
 
 JumpAgain = 0
 
+
+MoveTime = 0.0
+MoveHeight = 0
+MovePower = 10.0
+MoveKeyPressed = 0
+
 '''  '''
 
 
@@ -29,7 +35,7 @@ def Jump():
         return
     
     JumpHeight = (JumpTime * JumpTime - JumpPower * JumpTime) / 2.0
-    JumpTime += 0.11
+    JumpTime += 0.2
 
     if JumpTime > JumpPower and y - JumpHeight <= 100:
         JumpTime = 0
@@ -37,6 +43,18 @@ def Jump():
         JumpHeight = 0
         JumpKeyPressed = False
         JumpAgain = False
+
+def Move():
+    global MoveKeyPressed, MoveHeight, MovePower, MoveTime
+    # height에 상한을 정하고, 최대 속력을 맞추기
+
+    MoveHeight = (MoveTime * MoveTime - MovePower * MoveTime) / 5.0
+    # 변곡점때, 
+    MoveTime
+
+
+
+
 
 def handle_events():
     global JumpKeyPressed, JumpHeight, JumpPower, JumpTime
@@ -62,13 +80,25 @@ def handle_events():
 open_canvas(1280, 720)
 
 black_rect = load_image('black_rect.png')
+hero = load_image('knight_hero.png')
 
 running = 1
+x_frame = 0
+y_frame = 15
+count = 0
 
 if __name__ == '__main__':
     while running:
         clear_canvas()
-        black_rect.draw(x, y - JumpHeight)
+        black_rect.draw(x, y - JumpHeight, 60, 100)
+        # draw(Xpos for start, Ypos for start, WIDTH /none, HEIGHT /none)
+        # hero? 128x128
+
+        # hero.clip_draw(128 * x_frame, 128 * y_frame, 128, 128, x, y - JumpHeight)
+        count += 1
+        if count == 30:
+            x_frame = (x_frame + 1) % 16
+            count = 0
         update_canvas()
 
         handle_events()
--- test_pyg+box2d.py
+++ test_pyg+box2d.py
@@ -2,35 +2,48 @@ import pygame
 from pygame.locals import *
 from Box2D import *
 from Box2D.b2 import *
+from pico2d import *
 
 SCREEN_WD = 400
 SCREEN_HT = 400
 TARGET_FPS = 60
-PPM = 20.0
+PPM = 1.0
 
 screen = pygame.display.set_mode((SCREEN_WD, SCREEN_HT), 0, 32)
 pygame.display.set_caption("Pygame_Example")
 clock = pygame.time.Clock()
 
-world = b2World(gravity = (0, -10), doSleep = True)
+open_canvas(SCREEN_WD, SCREEN_HT)
 
+ # 중력 벡터 선언, sleep 여부 지정가능
+ # 월드객체 생성
+world = b2World(gravity = (0, -9.8), doSleep = True)
+
+ # ground box 정의
 ground1BodyDef = b2BodyDef()
-ground1BodyDef.position.Set(0, 1)
+ # type을 정해주지 않으면 기본 ground box로 지정됨
+ # ground1BodyDef.type = b2_dynamicBody
+ground1BodyDef.position.Set(0, 20)
+ # body 정의 : position, damping, etc
 ground1Body = world.CreateBody(ground1BodyDef)
+ # 월드객체를 이용, body 생성
 ground1Shape = b2PolygonShape()
-ground1Shape.SetAsBox(50, 5)
+ground1Shape.SetAsBox(400, 100)
+
 ground1Body.CreateFixture(shape = ground1Shape)
 
 box1BodyDef = b2BodyDef()
 box1BodyDef.type = b2_dynamicBody
-box1BodyDef.position.Set(10, 15)
+box1BodyDef.position.Set(200, 300)
 box1Body = world.CreateBody(box1BodyDef)
 box1Shape = b2PolygonShape()
-box1Shape.SetAsBox(2, 1)
+box1Shape.SetAsBox(40, 20)
+
 box1FixtureDef = b2FixtureDef()
 box1FixtureDef.shape = box1Shape
 box1FixtureDef.density = 1
 box1FixtureDef.friction = 0.3
+
 box1Body.CreateFixture(box1FixtureDef)
 
 timeStep = 1.0 / 60
@@ -41,6 +54,7 @@ colors = {
     dynamicBody : (127, 127, 127, 255),
     staticBody : (255, 255, 255, 255)
 }
+
 
 running = True
 while running:
@@ -58,6 +72,7 @@ while running:
         for fixture in body.fixtures:
             shape = fixture.shape
             vertices = [(body.transform * v) * PPM for v in shape.vertices]
+            print(vertices)
             vertices = [(v[0], (SCREEN_HT - v[1])) for v in vertices]
             pygame.draw.polygon(screen, colors[body.type], vertices)
     
@@ -65,5 +80,17 @@ while running:
     pygame.display.flip()
     clock.tick(TARGET_FPS)
 
+
+
+    clear_canvas()
+    #draw_rectangle()
+    update_canvas()
+
+    delay(0.01)
+    get_events()    
+
+
 pygame.quit()
 print("done")
+
+close_canvas()
